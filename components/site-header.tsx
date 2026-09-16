bc745087639fe118df08262dd5174fc3832744db
digitalsolution1-a <digital.solutions@brickslimited.com>
2026-09-16 16:15:58 +0100

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -45,7 +45,7 @@ export function SiteHeader() {
       <div className="mx-auto flex h-20 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
         <Link href="/" className="flex items-center gap-3">
           <Image
-            src="/images/Elgan Logo 2.jpg"
+            src="/images/elganlogocheck.png"
             alt="ELGAN Logo"
             width={140}
             height={40}
